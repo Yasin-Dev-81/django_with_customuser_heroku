a263c8ce1d61881c3c447c3b7d52fc60bc500b54
Yasin Dev 81 <76453371+Yasin-Dev-81@users.noreply.github.com>
2022-08-25 07:22:58 +0430

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,12 @@ from pathlib import Path
 from environs import Env
 import os
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # for environment variable
 env = Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
